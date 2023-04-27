c7a3d39692008dc81a99d8518197149163a7eed3
GrowlingM1ke <michallange1995@gmail.com>
2023-04-27 14:06:58 -0400

    Added concatenated cls token

--- model.py
+++ model.py
@@ -29,14 +29,15 @@ class AttentionModel(nn.Module):
           param.requires_grad = False
     
 
-  def forward(self, input_ids=None, attention_mask=None, labels=None):
+  def forward(self, input_ids=None, attention_mask=None, labels=None, loss=True):
     #Extract outputs from the body
     outputs_base = self.model_base(input_ids=input_ids, attention_mask=attention_mask)['last_hidden_state']
     outputs_sentiment = self.model_sentiment(input_ids=input_ids, attention_mask=attention_mask)['last_hidden_state']
     
     x = self.attn_layer(outputs_base, outputs_sentiment, attention_mask)
     x = self.ffn_layer(x)
-    x = self.loss_fn(x, labels.unsqueeze(1).float())
+    if loss:
+      x = self.loss_fn(x, labels.unsqueeze(1).float())
     
     return x
   
@@ -138,3 +139,42 @@ class AttentionLayerSingle(nn.Module):
       final_representation = torch.mul(att_token, outputs_base).sum(dim=1)
 
       return final_representation
+  
+
+class TwoCLSModel(nn.Module):
+  def __init__(self, checkpoint_base, checkpoint_sentiment, num_labels, freeze_base=True, device='cuda'): 
+    super(TwoCLSModel,self).__init__() 
+    self.num_labels = num_labels 
+
+    #Load Model with given checkpoint and extract its body
+    base_config = AutoConfig.from_pretrained(checkpoint_base, output_attentions=False,output_hidden_states=False)
+    self.model_base = AutoModel.from_pretrained(checkpoint_base,config=base_config)
+    sentiment_config = AutoConfig.from_pretrained(checkpoint_sentiment, output_attentions=False,output_hidden_states=False)
+    self.model_sentiment = AutoModel.from_pretrained(checkpoint_sentiment,config=sentiment_config)
+
+    # Fully Connected Layer for classification
+    self.ffn_layer = nn.Linear(2*768, 768)
+    self.ffn_layer2 = nn.Linear(768, 1)
+    # Loss function
+    self.loss_fn = nn.BCEWithLogitsLoss()
+    
+    if freeze_base:
+      for name, param in self.model_base.named_parameters():
+          param.requires_grad = False
+      for name, param in self.model_sentiment.named_parameters():
+          param.requires_grad = False
+
+  def forward(self, input_ids=None, attention_mask=None, labels=None, loss=True):
+    #Extract outputs from the body
+    outputs_base = self.model_base(input_ids=input_ids, attention_mask=attention_mask)['last_hidden_state'][:,0,:] #CLS token
+    outputs_sentiment = self.model_sentiment(input_ids=input_ids, attention_mask=attention_mask)['last_hidden_state'][:,0,:] #CLS token
+
+    # Concatenate the outputs of both models
+    x = torch.cat((outputs_base, outputs_sentiment), dim=1)
+    
+    x = torch.sigmoid(self.ffn_layer(x))
+    x = self.ffn_layer2(x)
+    if loss:
+      x = self.loss_fn(x, labels.unsqueeze(1).float())
+    
+    return x
